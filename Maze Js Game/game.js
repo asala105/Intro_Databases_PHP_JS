eff025d6153b701be8171c4e2164d29033a23b2b
Asala El Ghandour <ame105@mail.aub.edu>
2021-09-02 12:54:42 +0300

    maze game code

---
+++ Maze Js Game/game.js
@@ -0,0 +1,41 @@
+var YouLosevar = false;
+var score = 0;
+var GameOvervar = false;
+var GameStartvar = false;
+
+function GameStart(){
+    console.log("the game started!");
+    GameStartvar = true;
+    GameOvervar = false;
+    YouLosevar = false;
+    elements = document.getElementsByClassName("boundary");
+    for(var i = 0; i <elements.length; i++){
+            elements[i].classList.remove("youlose");
+    }    
+}
+
+function youLose(){
+    if (GameStartvar == true && GameOvervar == false && YouLosevar == false){
+        elements = document.getElementsByClassName("boundary");
+        for(var i = 0; i <elements.length; i++){
+            elements[i].classList.add("youlose");
+        }
+        YouLosevar = true;
+        GameOvervar = true;
+        GameStartvar = false;
+        score = score - 10;
+        document.getElementById("status").innerHTML = "You Lose! and your score is " + score  + " Click on S to start again";
+/*         for(var i = 0; i <elements.length; i++){
+            elements[i].classList.remove("youlose");
+        } */
+    }
+}
+
+function GameOver(){
+    if(!YouLosevar && !GameOvervar && GameStartvar){
+        score = score + 5;
+        document.getElementById("status").innerHTML = "You win! and your score is " + score + " Click on S to start again";
+        GameOvervar = true;
+        GameStartvar = true;
+    }
+}
